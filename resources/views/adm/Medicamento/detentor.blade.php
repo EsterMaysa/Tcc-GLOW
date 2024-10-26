7cb6263d8caba19d76b1758ab731882129a8aa49
gabidanety <gabinete878@gmail.com>
2024-10-25 22:50:08 -0300

    arrumando o detentor e meedicamento -- gaby

--- resources/views/adm/Medicamento/detentor.blade.php
+++ resources/views/adm/Medicamento/detentor.blade.php
@@ -34,21 +34,14 @@
             </table>
         </div>
     </div>
-    
+
     <table class="table table-bordered">
         <thead>
             <tr>
                 <th>Nome</th>
                 <th>CNPJ</th>
                 <th>Email</th>
-                <!-- <th>Logradouro</th>
-                <th>Bairro</th>
-                <th>Cidade</th>
-                <th>Estado</th>
-                <th>UF</th>
-                <th>CEP</th>
-                <th>Número</th>
-                <th>Complemento</th> -->
+
                 <th>Situação</th>
                 <th>Data</th>
                 <th>Ações</th>
@@ -60,54 +53,82 @@
                 <td>{{ $d->nomeDetentor }}</td>
                 <td>{{ $d->cnpjDetentor }}</td>
                 <td>{{ $d->emailDetentor }}</td>
-                <!-- <td>{{ $d->logradouroDetentor }}</td>
-                <td>{{ $d->bairroDetentor }}</td>
-                <td>{{ $d->cidadeDetentor }}</td>
-                <td>{{ $d->estadoDetentor }}</td>
-                <td>{{ $d->ufDetentor }}</td>
-                <td>{{ $d->cepDetentor }}</td>
-                <td>{{ $d->numeroDetentor }}</td>
-                <td>{{ $d->complementoDetentor }}</td> -->
-                <td>{{ $d->situacaoDetentor }}</td>
+                <td>
+                    @if($d->situacaoDetentor === 'A')
+                    Ativado
+                    @elseif($d->situacaoDetentor === 'D')
+                    Desativado
+                    @else
+                    Indefinido
+                    @endif
+                </td>
                 <td>{{ \Carbon\Carbon::parse($d->dataCadastroDetentor)->format('d/m/Y') }}</td>
 
                 <!-- Botão para abrir o modal -->
+
+                @if ($d->situacaoDetentor == 'A')
+
+                <td>
+
+                    <a href="{{ route('detentor.edit', $d->idDetentor) }}" class="btn btn-warning">Editar</a>
+                </td>
+
+                <td>
+
+                    <form action="{{ route('desativarDetentor', $d->idDetentor) }}" method="POST" style="display:inline;background-color: red;">
+                        @csrf
+                        @method('PUT') <!-- Use PUT para indicar que você está atualizando um recurso -->
+                        <button type="submit" class="btn btn-warning" onclick="return confirm('Tem certeza que deseja desativar este detentor?');">
+                            Desativar
+                        </button>
+                    </form>
+
+                </td>
+                @endif
                 <td>
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDetalhes{{ $d->idDetentor }}">
                         Ver mais
                     </button>
                 </td>
+
             </tr>
 
             <!-- Modal para os detalhes -->
             <div class="modal fade" id="modalDetalhes{{ $d->idDetentor }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Detalhes do Detentor</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <p><strong>Nome:</strong> {{ $d->nomeDetentor }}</p>
-                    <p><strong>CNPJ:</strong> {{ $d->cnpjDetentor }}</p>
-                    <p><strong>Email:</strong> {{ $d->emailDetentor }}</p>
-                    <p><strong>Logradouro:</strong> {{ $d->logradouroDetentor }}</p>
-                    <p><strong>Bairro:</strong> {{ $d->bairroDetentor }}</p>
-                    <p><strong>Cidade:</strong> {{ $d->cidadeDetentor }}</p>
-                    <p><strong>Estado:</strong> {{ $d->estadoDetentor }}</p>
-                    <p><strong>UF:</strong> {{ $d->ufDetentor }}</p>
-                    <p><strong>CEP:</strong> {{ $d->cepDetentor }}</p>
-                    <p><strong>Número:</strong> {{ $d->numeroDetentor }}</p>
-                    <p><strong>Complemento:</strong> {{ $d->complementoDetentor }}</p>
-                    <p><strong>Situação:</strong> {{ $d->situacaoDetentor }}</p>
-                    <p><strong>Data de Cadastro:</strong> {{ \Carbon\Carbon::parse($d->dataCadastroDetentor)->format('d/m/Y') }}</p>
-                  </div>
-                  <div class="modal-footer">
-                  <a href="{{ route('detentor.edit', $d->idDetentor) }}" class="btn btn-warning">Editar</a>
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                  </div>
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Detalhes do Detentor</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p><strong>Nome:</strong> {{ $d->nomeDetentor }}</p>
+                            <p><strong>CNPJ:</strong> {{ $d->cnpjDetentor }}</p>
+                            <p><strong>Email:</strong> {{ $d->emailDetentor }}</p>
+                            <p><strong>Logradouro:</strong> {{ $d->logradouroDetentor }}</p>
+                            <p><strong>Bairro:</strong> {{ $d->bairroDetentor }}</p>
+                            <p><strong>Cidade:</strong> {{ $d->cidadeDetentor }}</p>
+                            <p><strong>Estado:</strong> {{ $d->estadoDetentor }}</p>
+                            <p><strong>UF:</strong> {{ $d->ufDetentor }}</p>
+                            <p><strong>CEP:</strong> {{ $d->cepDetentor }}</p>
+                            <p><strong>Número:</strong> {{ $d->numeroDetentor }}</p>
+                            <p><strong>Complemento:</strong> {{ $d->complementoDetentor }}</p>
+                            <p><strong>Situação:</strong> {{ $d->situacaoDetentor }}</p>
+                            <p><strong>Data de Cadastro:</strong> {{ \Carbon\Carbon::parse($d->dataCadastroDetentor)->format('d/m/Y') }}</p>
+                        </div>
+                        <div class="modal-footer">
+                            <a href="{{ route('detentor.edit', $d->idDetentor) }}" class="btn btn-warning">Editar</a>
+                            <form action="{{ route('desativarDetentor', $d->idDetentor) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('PUT') <!-- Use PUT para indicar que você está atualizando um recurso -->
+                                <button type="submit" class="btn btn-warning" onclick="return confirm('Tem certeza que deseja desativar este detentor?');">
+                                    Desativar
+                                </button>
+                            </form>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
             @endforeach
         </tbody>
