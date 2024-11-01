9e292c0eab6afe831b9439429748db22266e717a
gabidanety <gabinete878@gmail.com>
2024-10-31 22:45:44 -0300

    gaby

--- resources/views/farmacia/Medicamento/cadMedicamento.blade.php
+++ resources/views/farmacia/Medicamento/cadMedicamento.blade.php
@@ -1,71 +1,70 @@
 @include('includes.headerFarmacia')
 <div class="container" style="margin: 20px;">
 
-<form action="/CadMedFarma" method="POST">
-    @csrf
+    <form action="/CadMedFarma" method="POST">
+        @csrf
 
-    <!-- Código de Barras do Medicamento -->
-    <div class="form-group">
-        <label for="codigoDeBarrasMedicamento">Código de Barras</label>
-        <input type="text" class="form-control" id="codigoDeBarrasMedicamento" name="codigoDeBarrasMedicamento" required onblur="buscarMedicamento()">
-    </div>
+        <!-- Código de Barras do Medicamento -->
+        <div class="form-group">
+            <label for="codigoDeBarrasMedicamento">Código de Barras</label>
+            <input type="text" class="form-control" id="codigoDeBarrasMedicamento" name="codigoDeBarrasMedicamento" value="{{ old('codigoDeBarrasMedicamento') }}" required onblur="buscarMedicamento()">
+        </div>
 
-    <!-- Nome do Medicamento -->
-    <div class="form-group">
-        <label for="nomeMedicamento">Nome do Medicamento</label>
-        <input type="text" class="form-control" id="nomeMedicamento" name="nomeMedicamento" required>
-    </div>
+        <!-- Nome do Medicamento -->
+        <div class="form-group">
+            <label for="nomeMedicamento">Nome do Medicamento</label>
+            <input type="text" class="form-control" id="nomeMedicamento" name="nomeMedicamento" value="{{ old('nomeMedicamento') }}" required>
+        </div>
 
-    <!-- Nome Genérico do Medicamento -->
-    <div class="form-group">
-        <label for="nomeGenericoMedicamento">Nome Genérico do Medicamento</label>
-        <input type="text" class="form-control" id="nomeGenericoMedicamento" name="nomeGenericoMedicamento" required>
-    </div>
+        <!-- Nome Genérico do Medicamento -->
+        <div class="form-group">
+            <label for="nomeGenericoMedicamento">Nome Genérico do Medicamento</label>
+            <input type="text" class="form-control" id="nomeGenericoMedicamento" name="nomeGenericoMedicamento" value="{{ old('nomeGenericoMedicamento') }}" required>
+        </div>
+        <!-- Validade do Medicamento -->
+        <div class="form-group">
+            <label for="validadeMedicamento">Data de Validade</label>
+            <input type="date" class="form-control" id="validadeMedicamento" name="validadeMedicamento" required>
+        </div>
 
-    <!-- Validade do Medicamento -->
-    <div class="form-group">
-        <label for="validadeMedicamento">Data de Validade</label>
-        <input type="date" class="form-control" id="validadeMedicamento" name="validadeMedicamento" required>
-    </div>
+        <!-- Lote do Medicamento -->
+        <div class="form-group">
+            <label for="loteMedicamento">Lote</label>
+            <input type="text" class="form-control" id="loteMedicamento" name="loteMedicamento" required>
+        </div>
 
-    <!-- Lote do Medicamento -->
-    <div class="form-group">
-        <label for="loteMedicamento">Lote</label>
-        <input type="text" class="form-control" id="loteMedicamento" name="loteMedicamento" required>
-    </div>
+        <!-- Dosagem do Medicamento -->
+        <div class="form-group">
+            <label for="dosagemMedicamento">Dosagem</label>
+            <input type="text" class="form-control" id="dosagemMedicamento" name="dosagemMedicamento" required>
+        </div>
 
-    <!-- Dosagem do Medicamento -->
-    <div class="form-group">
-        <label for="dosagemMedicamento">Dosagem</label>
-        <input type="text" class="form-control" id="dosagemMedicamento" name="dosagemMedicamento" required>
-    </div>
-
-    <!-- Forma Farmacêutica do Medicamento -->
-    <div class="form-group">
+        <!-- Forma Farmacêutica do Medicamento -->
+        <div class="form-group">
             <label for="formaMedicamento">Forma Farmacêutica</label>
             <select class="form-control" id="formaMedicamento" name="forma" required>
                 <option value="">Selecione a Forma Farmacêutica</option>
                 <option value="Comprimido">Comprimido</option>
                 <option value="Cápsula">Cápsula</option>
-                <option value="Pomada" >Pomada</option>
-                <option value="Solução" >Solução</option>
-                <option value="Suspensão" >Suspensão</option>
-                <option value="Creme" >Creme</option>
-                <option value="Gel" >Gel</option>
-                <option value="Injeção" >Injeção</option>
+                <option value="Pomada">Pomada</option>
+                <option value="Solução">Solução</option>
+                <option value="Suspensão">Suspensão</option>
+                <option value="Creme">Creme</option>
+                <option value="Gel">Gel</option>
+                <option value="Injeção">Injeção</option>
             </select>
         </div>
 
-    <!-- Composição do Medicamento -->
-    <div class="form-group">
-        <label for="composicaoMedicamento">Composição</label>
-        <textarea class="form-control" id="composicaoMedicamento" name="composicaoMedicamento" required></textarea>
-    </div>
-    
+        <!-- Composição do Medicamento -->
+        <div class="form-group">
+            <label for="composicaoMedicamento">Composição</label>
+            <textarea class="form-control" id="composicaoMedicamento" name="composicaoMedicamento" required></textarea>
+        </div>
 
-    <!-- Botão de Enviar -->
-    <button type="submit" class="btn btn-primary">Cadastrar Medicamento</button>
-</form>
+
+        <!-- Botão de Enviar -->
+        <button type="submit" class="btn btn-primary">Cadastrar Medicamento</button>
+    </form>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
@@ -77,20 +76,17 @@
                 url: `/CodMed/${codigoDeBarras}`,
                 type: 'GET',
                 success: function(data) {
-                    console.log(data); // Adicione isso para ver o que está retornando
-
+                    console.log(data); // Veja o que está sendo retornado
                     if (data) {
                         $('#nomeMedicamento').val(data.nomeMedicamento);
                         $('#nomeGenericoMedicamento').val(data.nomeGenericoMedicamento);
                         $('#composicaoMedicamento').val(data.composicaoMedicamento);
 
-                        // Bloqueia o campo de código de barras
-                        $('#codigoDeBarrasMedicamento').prop('disabled', true);
-                        $('#nomeMedicamento').prop('disabled', true);
-                        $('#nomeGenericoMedicamento').prop('disabled', true);
-                        $('#composicaoMedicamento').prop('disabled', true);
-
-
+                        // Remover a desabilitação dos campos
+                        // $('#codigoDeBarrasMedicamento').prop('disabled', true);
+                        // $('#nomeMedicamento').prop('disabled', true);
+                        // $('#nomeGenericoMedicamento').prop('disabled', true);
+                        // $('#composicaoMedicamento').prop('disabled', true);
                     } else {
                         alert('Código de barras não encontrado!');
                     }
@@ -98,6 +94,7 @@
                 error: function() {
                     alert('Erro ao buscar o medicamento!');
                 }
+
             });
         }
     }
