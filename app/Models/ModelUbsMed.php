3a3437643964ae8371dd7f5841b67cdf4127457b
gabidanety <gabinete878@gmail.com>
2024-11-16 21:07:30 -0300

    insert da tabela nova

--- app/Models/ModelUbsMed.php
+++ app/Models/ModelUbsMed.php
@@ -12,8 +12,8 @@ class ModelUbsMed extends Model
     // Define a tabela que este model irá manipular
     protected $table = 'tbubsmed';
 
-    // Define a chave primária, caso não seja o padrão (id)
-    protected $primaryKey = ['idMedicamento', 'idUBS']; // Caso sua chave primária seja composta
+    // Caso a chave primária seja composta, remova este atributo
+    protected $primaryKey = 'id'; 
 
     // Desabilitar a atribuição em massa para segurança
     protected $guarded = [];
@@ -21,19 +21,21 @@ class ModelUbsMed extends Model
     // Indica que a chave primária não é auto incremento
     public $incrementing = false;
 
-    // Define o tipo de dados para a chave primária composta
-    protected $keyType = 'string'; // Caso seus ids sejam inteiros, não é necessário definir
+    // Define o tipo de dados para a chave primária
+    protected $keyType = 'string';
 
-    public $timestamps = false; // Caso não tenha os campos created_at e updated_at
+    // Desabilitar timestamps, caso não tenha os campos created_at e updated_at
+    public $timestamps = false;
 
+    // Relacionamento com medicamentos
     public function medicamento()
     {
         return $this->belongsTo(MedicamentoModel::class, 'idMedicamento', 'idMedicamento');
     }
+
+    // Relacionamento com UBS
     public function ubs()
     {
         return $this->belongsTo(UBSModel::class, 'idUBS', 'idUBS');
     }
-
-
 }
